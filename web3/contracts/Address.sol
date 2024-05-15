ddc0f943ba8960f5d12cf43bb3120a53264ecb2b
Daniel Magro <dando385@yahoo.com>
2024-05-15 17:50:44 -0400

    initial commit

---
+++ web3/contracts/Address.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+library address {
+    function isContract(address account) internal view returns (bool) {
+        return account.code.length > 0;
+    }
+
+    function sendValue(address payable recipient, uint256 amount) internal {
+        require(address(this).balance >= amount, "Address has insuffient balance");
+        (bool success, ) = recipient.call{value: amount}("");
+        require(success, "Address unable to send value, recipient may have reverted");
+    }
+
+    function functionCall(address target, bytes memory data) internal returns (bytes memory) {
+        return 
+    }
+}
